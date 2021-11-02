f7f0a376b03f77518f0bb771845f5a87e2c2d834
Alexandre Capt <acapt@adobe.com>
2021-11-02 15:31:27 +0100

    fix: video not preloaded

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -6,12 +6,24 @@ export default function decorate(block) {
   }
   const poster = block.querySelector('img') ? `poster="${block.querySelector('img').src}"` : '';
   const a = block.querySelector('a');
-  const videoSrc = a.href;
+
+  const href = a.getAttribute('href');
+  const url = new URL(href);
+  const { hostname } = url;
+  let { pathname } = url;
+
+  if (hostname.includes('hlx.blob.core')) {
+    // transform links from blob
+    const helixId = pathname.split('/')[2];
+    const type = href.includes('.mp4') ? 'mp4' : 'gif';
+    pathname = `/media_${helixId}.${type}`;
+  }
+
   const video = document.createElement('div');
   const figure = buildFigure(block.firstChild.firstChild);
   video.classList.add('video-wrapper');
-  video.innerHTML = `<video controls preload="none" ${poster}>
-    <source src="${videoSrc}" type="video/mp4">
+  video.innerHTML = `<video controls ${poster}>
+    <source src=".${pathname}" type="video/mp4">
   </video>`;
   block.innerHTML = '<figure class="figure"></figure>';
   block.firstChild.prepend(video);
