66ab4f0ff6b755d3288fa44fa14c5b3c54a93025
Lars Trieloff <trieloff@adobe.com>
2021-12-13 12:09:01 +0100

    feat(rum): rum collect which tags are shown in the component

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,10 +1,15 @@
+import { sampleRUM } from '../../scripts/scripts.js';
+
 export default function decorateTags(blockEl) {
   const tags = blockEl.querySelectorAll('a');
   const container = blockEl.querySelector('p');
   container.classList.add('tags-container');
   container.textContent = '';
-  tags.forEach((tag) => {
+  const target = Array.from(tags).reduce((targets, tag) => {
     tag.classList.add('button');
     container.append(tag);
-  });
+    targets.push(tag.href);
+    return targets;
+  }, []).join('; ');
+  sampleRUM('loadtags', { target, source: `.${blockEl.getAttribute('data-block-name')}` });
 }
