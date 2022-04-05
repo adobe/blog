3c51e3ed8cabf2a1ab7342aa2fef98200da61e83
William Ste-Marie <williambsm@hotmail.fr>
2022-04-05 11:59:30 -0400

    fix(table_of_content): fix trailing url

--- blocks/table-of-content/table-of-content.js
+++ blocks/table-of-content/table-of-content.js
@@ -3,23 +3,24 @@ import createTag from '../gnav/gnav-utils.js';
 export default function decorate(block) {
     block.insertAdjacentHTML('beforebegin', '<hr/>');
     block.insertAdjacentHTML('afterend', '<hr/>');
+    const baseURL = window.location.href;
     const links = block.querySelectorAll('li');
     const gnavHeight = parseInt(window.getComputedStyle(document.querySelector('header')).height.replace('px', ''));
 
     for(let i = 0; i < links.length; i ++) {
         const title = links[i].innerText;
         const linkSlug = stringCleanup(title);
-        let anchorElement = createTag('a', {href: `${window.location.href}#${linkSlug}`});
+        let anchorElement = createTag('a', {href: `${baseURL}#${linkSlug}`});
         links[i].innerText = '';
         links[i].append(anchorElement);
         anchorElement.innerText = title;
 
         if (title === 'Introduction') {
             const targetNode = block.parentElement.parentElement.previousSibling;
-            generateLink(links, links[i], targetNode, gnavHeight);
+            generateLink(links[i], targetNode, gnavHeight);
         } else {
             const targetNode = document.getElementById(linkSlug);
-            generateLink(links, links[i], targetNode, gnavHeight);
+            generateLink(links[i], targetNode, gnavHeight);
         }
     }
 }
@@ -28,8 +29,9 @@ function stringCleanup(string) {
     return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim().replaceAll(' ', '-');
 }
 
-function generateLink(links, link, targetNode, height) {
-    link.addEventListener('click', () => {
+function generateLink(link, targetNode, height) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
         const targetOffset = targetNode.offsetTop - height - 24;
         window.scrollTo({
             top: targetOffset,
