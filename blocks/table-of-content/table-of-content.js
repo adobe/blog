bd47b4b230b6c2fb4d4222d4ef626512641acdaf
William Ste-Marie <williambsm@hotmail.fr>
2022-04-05 11:41:13 -0400

    feat(table_of_contents): creation of the block

---
+++ blocks/table-of-content/table-of-content.js
@@ -0,0 +1,55 @@
+import {
+    createOptimizedPicture,
+    normalizeHeadings,
+} from '../../scripts/scripts.js';
+import createTag from '../gnav/gnav-utils.js';
+  
+export default function decorate(block) {
+    block.insertAdjacentHTML('beforebegin', '<hr/>');
+    block.insertAdjacentHTML('afterend', '<hr/>');
+    const links = block.querySelectorAll('li');
+    const gnavHeight = parseInt(window.getComputedStyle(document.querySelector('header')).height.replace('px', ''));
+
+    for(let i = 0; i < links.length; i ++) {
+        const title = links[i].innerText;
+        const linkSlug = stringCleanup(title);
+        let anchorElement = createTag('a', {href: `${window.location.href}#${linkSlug}`});
+        links[i].innerText = '';
+        links[i].append(anchorElement);
+        anchorElement.innerText = title;
+
+        if (title === 'Introduction') {
+            const targetNode = block.parentElement.parentElement.previousSibling;
+            generateLink(links, links[i], targetNode, gnavHeight);
+        } else {
+            const targetNode = document.getElementById(linkSlug);
+            generateLink(links, links[i], targetNode, gnavHeight);
+        }
+    }
+}
+
+function stringCleanup(string) {
+    return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim().replaceAll(' ', '-');
+}
+  
+function updateLinksClass(links, targetLink) {
+    for(let i = 0; i < links.length; i ++) {
+        if (links[i] != targetLink) {
+            links[i].classList.remove('active');
+        }
+    }
+
+    targetLink.classList.add('active');
+}
+
+function generateLink(links, link, targetNode, height) {
+    link.addEventListener('click', () => {
+        const targetOffset = targetNode.offsetTop - height - 24;
+        updateLinksClass(links, link);
+        window.scrollTo({
+            top: targetOffset,
+            left: 0,
+            behavior: 'smooth'
+        });
+    })
+}
