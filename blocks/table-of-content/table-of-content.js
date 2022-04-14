bf02d1d1409a7b98438ecdf5a9cd229c9a3a1b04
William Ste-Marie <williambsm@hotmail.fr>
2022-04-14 14:09:28 -0400

    feat(table-of-contents): v2

--- blocks/table-of-content/table-of-content.js
+++ blocks/table-of-content/table-of-content.js
@@ -1,45 +1,35 @@
 import createTag from '../gnav/gnav-utils.js';
-  
-export default function decorate(block) {
-    block.insertAdjacentHTML('beforebegin', '<hr/>');
-    block.insertAdjacentHTML('afterend', '<hr/>');
-
-    const url = new URL(window.location.href);
-    const baseUrl = url.origin + url.pathname;
-    const $tabs = block.querySelectorAll('li');
-    const gnavHeight = parseInt(window.getComputedStyle(document.querySelector('header')).height.replace('px', ''));
-
-    $tabs.forEach(($tab) => {
-        const title = $tab.textContent;
-        const id = stringCleanup(title);
-        const href = `${baseUrl}#${id}`;
-        const $anchor = createTag('a', { href });
-        let target;
-
-        $tab.innerHTML = '';
-        $tab.append($anchor);
-        $anchor.textContent = title;
-
-        if (title === 'Introduction') {
-            target = block.parentElement.parentElement.previousSibling;
-        } else {
-            target = document.getElementById(id);
-        }
-
-        $tab.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            window.scrollTo({
-                top: target.offsetTop - gnavHeight - 24,
-                left: 0,
-                behavior: 'smooth'
-            });
-
-            window.history.pushState({},'', href);
-        });
-    });
-}
 
 function stringCleanup(string) {
-    return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim().replaceAll(' ', '-');
+  return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim().replaceAll(' ', '-');
+}
+
+export default function decorate(block) {
+  block.insertAdjacentHTML('beforebegin', '<hr/>');
+  block.insertAdjacentHTML('afterend', '<hr/>');
+
+  const url = new URL(window.location.href);
+  const baseUrl = url.origin + url.pathname;
+  const $tabs = block.querySelectorAll('li');
+
+  $tabs.forEach(($tab) => {
+    const title = $tab.textContent;
+    const id = stringCleanup(title);
+    const href = `${baseUrl}#${id}`;
+    const $anchor = createTag('a', { href }, null);
+    let target;
+
+    $tab.innerHTML = '';
+    $tab.append($anchor);
+    $anchor.textContent = title;
+
+    if (title === 'Introduction') {
+      target = block.parentElement.parentElement.previousSibling;
+      target.id = id;
+    } else {
+      target = document.getElementById(id);
+    }
+
+    target.classList.add('--with-scroll-margin-top');
+  });
 }
