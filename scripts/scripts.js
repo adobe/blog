0878547ed6ff1d0fd9289916c41303bd075a44d5
Chris Millar <cmillar@adobe.com>
2022-05-14 11:28:26 -0600

    wip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -957,14 +957,15 @@ export async function loadBlock(block, eager = false) {
   if (!(block.getAttribute('data-block-status') === 'loading' || block.getAttribute('data-block-status') === 'loaded')) {
     block.setAttribute('data-block-status', 'loading');
     const blockName = block.getAttribute('data-block-name');
+    const milo = 'https://milo.pink/libs';
     try {
       const cssLoaded = new Promise((resolve) => {
-        loadCSS(`/blocks/${blockName}/${blockName}.css`, resolve);
+        loadCSS(`${milo}/blocks/${blockName}/${blockName}.css`, resolve);
       });
       const decorationComplete = new Promise((resolve) => {
         (async () => {
           try {
-            const mod = await import(`/blocks/${blockName}/${blockName}.js`);
+            const mod = await import(`${milo}/blocks/${blockName}/${blockName}.js`);
             if (mod.default) {
               await mod.default(block, blockName, document, eager);
             }
