28d26b845e11359677ba83c81cc07df078fe3fb2
William Ste-Marie <williambsm@hotmail.fr>
2022-05-24 16:20:21 -0400

    feat(table-of-contents): updated linking system to be more reliable

--- blocks/table-of-contents/table-of-contents.js
+++ blocks/table-of-contents/table-of-contents.js
@@ -1,35 +1,38 @@
 import { createTag } from '../block-helpers.js';
 
-function stringCleanup(string) {
-  return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim().replaceAll(' ', '-');
-}
-
 export default function decorate($block) {
   const url = new URL(window.location.href);
-  const baseUrl = url.origin + url.pathname;
+  const baseUrl = `${url.origin}${url.pathname}#`;
   const $list = createTag('ol', { }, null);
   const $content = $block.querySelector('ul');
+  const $headers = document.querySelectorAll('h1, h2, h3, h4, h5');
 
   Array.from($content.children).forEach(($tab) => {
-    const title = $tab.textContent;
-    const id = stringCleanup(title);
-    const href = `${baseUrl}#${id}`;
-    const $anchor = createTag('a', { href }, null);
+    const $anchor = createTag('a', {}, null);
     let target;
 
-    $tab.innerHTML = '';
-    $tab.append($anchor);
-    $anchor.textContent = title;
-    $list.append($tab);
-
-    if (title === 'Introduction') {
+    if ($tab.textContent === 'Introduction') {
+      $anchor.href = `${baseUrl}introduction`;
+      $anchor.textContent = 'Introduction';
       target = $block.parentElement.parentElement.previousSibling;
-      target.id = id;
     } else {
-      target = document.getElementById(id);
+      $headers.forEach(($header) => {
+        if ($tab.textContent === $header.textContent) {
+          $anchor.href = `${baseUrl}${$header.id}`;
+          $anchor.textContent = $tab.textContent;
+          target = $header;
+        }
+      });
     }
 
-    target.classList.add('--with-scroll-margin-top');
+    if (target) {
+      $tab.innerHTML = '';
+      $tab.append($anchor);
+      $list.append($tab);
+      target.classList.add('--with-scroll-margin-top');
+    } else {
+      $tab.remove();
+    }
   });
 
   $content.after($list);
