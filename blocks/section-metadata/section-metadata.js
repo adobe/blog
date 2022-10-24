6b2161f7b9593d9e46c8d1006bec9087821dcc1f
William Ste-Marie <williambsm@hotmail.fr>
2022-10-24 19:57:08 -0400

    feat(metadata): import section-metadata from milo

---
+++ blocks/section-metadata/section-metadata.js
@@ -0,0 +1,36 @@
+function handleBackground(div, section) {
+  const pic = div.querySelector('picture');
+  if (pic) {
+    section.classList.add('has-background');
+    pic.classList.add('section-background');
+    section.insertAdjacentElement('afterbegin', pic);
+  } else {
+    const color = div.textContent;
+    if (color) {
+      section.style.backgroundColor = color;
+    }
+  }
+}
+
+function handleStyle(div, section) {
+  const value = div.textContent.toLowerCase();
+  const styles = value.split(', ').map((style) => style.replaceAll(' ', '-'));
+  if (section) {
+    section.classList.add(...styles);
+  }
+}
+
+export default function decorate(el) {
+  const section = el.closest('.section-wrapper');
+  if (!section) return;
+  const keyDivs = el.querySelectorAll(':scope > div > div:first-child');
+  keyDivs.forEach((div) => {
+    const valueDiv = div.nextElementSibling;
+    if (div.textContent === 'style' && valueDiv.textContent) {
+      handleStyle(valueDiv, section);
+    }
+    if (div.textContent === 'background') {
+      handleBackground(valueDiv, section);
+    }
+  });
+}
