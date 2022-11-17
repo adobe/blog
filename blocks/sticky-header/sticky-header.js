c8beee7f5e01a0e778a6d37327261957923bcf50
casey tokarchuk <caseytokarchuk@gmail.com>
2022-11-17 15:03:56 -0500

    Added a close button to the sticky header

--- blocks/sticky-header/sticky-header.js
+++ blocks/sticky-header/sticky-header.js
@@ -1,8 +1,24 @@
+import { createTag } from '../block-helpers.js';
+
 export default function decorate($block) {
   const $header = document.querySelector('header');
+  const $stickyHeader = document.querySelector('.sticky-header.block')
+  const $stickyHeaderContents = document.querySelector('.sticky-header div')
+
   $header.parentNode.insertBefore($block, $header.nextSibling);
 
   const $a = $block.querySelector('a');
   $a.classList.add('button');
   $a.target = '_blank';
+
+  // Should these go at the top?
+  const $close = createTag('a', { class: 'sticky-header-close' });
+  const $closeIcon = createTag('img', { class: 'newsletter-modal-close-icon', src: '/blocks/sticky-header/close.svg' });
+
+  $close.addEventListener('click', (e) => {
+    $stickyHeader.remove()
+  })
+
+  $stickyHeaderContents.append($close)
+  $close.append($closeIcon)
 }
